f32de1821757d8aa317ad06d67e7af6a30d4ca4d
Jaques Dias <jaquesdias19@gmail.com>
2019-09-14 18:22:14 +0200

    Models validations and relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  has_many :ratings
+
+  validate :user
+  validate :category
+  validates :title, presence: true
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,14 @@
 class Rating < ApplicationRecord
+  belongs_to :movie
+
+  validates :rating_value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+  after_save :update_movie_rating
+
+  private
+
+  def update_movie_rating
+    rating_average = Rating.where(movie_id: movie_id).average(:rating_value).to_i
+    Movie.find(movie_id).update(rating_average: rating_average)
+  end
 end
