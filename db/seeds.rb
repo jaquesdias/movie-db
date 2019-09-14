e3163dff9a858f9ed7af0217d4dec3f4463401d8
Jaques Dias <jaquesdias19@gmail.com>
2019-09-14 18:24:02 +0200

    Create database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+  user_pass = Faker::Internet.password
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: user_pass, password_confirmation: user_pass)
+end
+
+categories = [
+  'Comedy',
+  'Sci-fi',
+  'Horror',
+  'Romance',
+  'Action',
+  'Thriller',
+  'Drama',
+  'Mistery',
+  'Crime',
+  'Animation',
+  'Adventure',
+  'Fantasy',
+  'Comedy-romance',
+  'Action-comedy',
+  'Superhero',
+]
+
+categories.each do |category|
+  Category.find_or_initialize_by(name: category).save
+end
+
+15.times do
+  movie = Movie.create(
+    title: Faker::Book.title,
+    description: Faker::Movie.quote,
+    category_id: Category.order('RANDOM()').first.id,
+    user_id: User.order('RANDOM()').first.id
+  )
+end
+
+100.times do
+  rating = Rating.find_or_initialize_by(
+    user_id: User.order('RANDOM()').first.id,
+    movie_id: Movie.order('RANDOM()').first.id
+  )
+
+  rating.rating_value = rand(0..5)
+  rating.save
+end
