d032b30682bf3331bb1ab1760865659bad5740bf
Jaques Dias <jaquesdias19@gmail.com>
2019-09-14 10:59:57 +0200

    Create Category model, database table, spec and factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_083527) do
+ActiveRecord::Schema.define(version: 2019_09_14_085402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
