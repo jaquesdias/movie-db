660e1df0b1d8dbca91b2c2d1ace399d6a36c4b56
Jaques Dias <jaquesdias19@gmail.com>
2019-09-14 20:49:09 +0200

    Create movie controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,69 @@
+class MoviesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_movie_and_categories, only: [:new, :show, :edit, :update, :destroy]
+  before_action :check_movie_user, only: [:edit, :update, :destroy]
+
+  def index
+    @movies_by_categories = Category.includes(:movies)
+    @movies_by_ratings = Movie.all.group_by(&:rating_average)
+    query_object = MovieQueryObject.new(params)
+    @movies = query_object.execute
+  end
+
+  def edit; end
+
+  def new; end
+
+  def create
+    movie = Movie.new(movie_params)
+    movie.user = current_user
+
+    if movie.save
+      flash[:notice] = 'Movie created with success'
+      redirect_to action: 'index'
+    else
+      flash[:error] = movie.errors.full_messages
+      redirect_to action: 'new'
+    end
+
+  end
+
+  def update
+    if @movie.update(movie_params)
+      flash[:notice] = 'Movie updated with success'
+      redirect_to action: 'index'
+    else
+      flash[:error] = @movie.errors.full_messages
+      redirect_to action: 'show'
+    end
+  end
+
+  def destroy
+    if @movie.destroy
+      flash[:notice] = 'Movie deleted with success'
+    else
+      flash[:error] = @movie.errors.full_messages
+    end
+
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def set_movie_and_categories
+    @movie = params[:id].present? ? Movie.find(params[:id]) : Movie.new
+    @categories = Category.order(:name).pluck(:name, :id)
+  end
+
+  def check_movie_user
+    unless @movie.user == current_user
+      flash[:error] = "You don't have permition to manipulate this movie data"
+
+      redirect_to root_path
+    end
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :category_id, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root 'movies#index'
 
-  resource :movies
+  resources :movies
 end
